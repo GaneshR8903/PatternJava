c62a5b3939c2f1ebe7acd1ba8b83267c0ed4e9ca
GaneshR8903 <ganesh.nexitance@gmail.com>
2024-07-13 20:38:56 +0530

    SquarePattern

--- Square.java
+++ Square.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Square{
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        try(Scanner sc=new Scanner(System.in)){
         System.out.print("Enter the number of rows:");
         int rows=sc.nextInt();
         for(int i=0;i<rows;i++){
@@ -13,6 +13,6 @@ public class Square{
                 System.out.println();
             }
         }
-        sc.close();
+    }
     }
 }
